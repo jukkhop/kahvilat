5d608ef86ef0cac6140da1b2f743f2f9e9681e50
Jukka Hopeavuori <jukka.hopeavuori@motleyagency.com>
2019-11-03 00:35:18 +0200

    Update circleci config

--- backend/test/kahvilat_backend/main_test.clj
+++ backend/test/kahvilat_backend/main_test.clj
@@ -1,6 +1,5 @@
 (ns kahvilat-backend.main-test
-  (:require [clojure.test :refer :all]
-            [kahvilat-backend.main :refer :all]))
+  (:require [clojure.test :refer :all]))
 
 (deftest a-test
   (testing "test basic arithmetic"
